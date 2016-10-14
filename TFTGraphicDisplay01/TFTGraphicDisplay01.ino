79ddff83b3e6581760f8a050934346ba88182074
roberto melendez <roberto.melendez@gmail.com>
2016-10-14 17:15:40 -0400

    Revert "Calculations for speed to the interrupt"
    This reverts commit 102c5c8c6a58a2d5f17dfefef0518a5c93bdfc4c.

--- TFTGraphicDisplay01/TFTGraphicDisplay01.ino
+++ TFTGraphicDisplay01/TFTGraphicDisplay01.ino
@@ -18,7 +18,7 @@ extern uint8_t BigFont[];
 //extern uint8_t Grotesk24x48[];
 extern uint8_t Ubuntu[];
 
-UTFT          myGLCD(CTE70, 25, 26, 27, 28);
+UTFT          myGLCD(CTE70, 25,26,27,28);
 #ifdef TOUCH
 URTouch        myTouch(6, 5, 32, 3, 2);
 
@@ -31,7 +31,6 @@ int counter = 0;
 int cruiseON = 0;
 uint32_t rc = 1;
 static int count = 0;
-static int loopIterations = 0;
 
 const char clearBuffer[] = "    ";
 char label[] = "0";
@@ -43,20 +42,20 @@ char oldLabel[] = "0";
 /// temperature related variables
 // temperature calibration buffer
 const float voltsPCount[5] = {0.51, 0.52, 0.53, 0.52, 0.53};
-const float calibrationScale[5] = {0.4396, 0.4318, 0.4465, 0.4164, 0.4318};
-const float calibrationOffset[5] = {27.459, 27.655, 29.452, 21.762, 26.181};
+const float calibrationScale[5] = {0.4396,0.4318,0.4465,0.4164,0.4318};
+const float calibrationOffset[5] = {27.459,27.655,29.452,21.762,26.181};
 int temperaturesInt[5];
 float temperatures[5];
 volatile int getTemperaturesBtn = 0;
-volatile int delta = 0;
 
 /*Speed input variables*/
-volatile int full_revolutions;
-int currentSpeed;
+volatile byte full_revolutions;
+float currentSpeed;
 unsigned long timeold;
-const int POT_IN = A0;
-const int POT_OUT = 10;
-const int RPM_InterruptPort = 8;
+int POT_IN = A0;
+int POT_OUT = 10;
+int RPM_InterruptPort = 8;
+int potentiometerValue;
 const float speedConversionValue = 0.064091;
 
 
@@ -74,7 +73,7 @@ void startTimer(Tc *tc, uint32_t channel, IRQn_Type irq, uint32_t frequency)
   tc->TC_CHANNEL[channel].TC_IER = TC_IER_CPCS;
   tc->TC_CHANNEL[channel].TC_IDR = ~TC_IER_CPCS;
   //pmc_set_writeprotect(true);
-  NVIC_EnableIRQ(irq);
+  NVIC_EnableIRQ(irq); 
 }
 
 void setup()
@@ -86,7 +85,6 @@ void setup()
   myGLCD.setFont(Ubuntu);
   myGLCD.print("RPM: ", 100, 72);
   myGLCD.print("Speed: ", 100, 144);
-  myGLCD.print("POT IN: ", 100, 220);
 
 #if TOUCH
   myTouch.InitTouch();
@@ -106,40 +104,40 @@ void setup()
   pinMode(POT_IN, INPUT);
   pinMode(POT_OUT, OUTPUT);
 
+  potentiometerValue = 0;
   full_revolutions = 0;
   currentSpeed = 0;
   timeold = 0;
-  Serial.println("time\tRPM\tfull_revolutions\tSpeed\tAnalog Read in");
 }
 
 void loop()
 {
-
-  Serial.print(millis(), DEC);
-  Serial.print('\t');
-  Serial.print(currentSpeed, 1);
-  Serial.print('\t');
-  Serial.print(full_revolutions);
-  Serial.print('\t');
-  Serial.print((int)currentSpeed * speedConversionValue);
-  Serial.print('\t');
-  Serial.println(sensorValue);
+  
   myGLCD.printNumI(count++, 0, 10);
-
+  
   sensorValue = analogRead(POT_IN);
   sensorValue = analogRead(POT_IN);
-  analogWrite(POT_OUT, map(sensorValue, 0, 1023, 0, 255));
-  /*
-    if (!cruiseON)
-    {
-      sensorValue = analogRead(sensorPin);
-      sensorValue = analogRead(sensorPin);
-    }*/
+  analogWrite(POT_OUT,map(sensorValue, 0,1023,0,255));
+  
+  
+  if (full_revolutions >= 10) {
+    detachInterrupt(RPM_InterruptPort);
+    int currentMillis = millis();
+    currentSpeed = (60.0 * 1000/(currentMillis - timeold) * full_revolutions);//*speedConversionValue;
+    timeold = currentMillis;
+    full_revolutions = 0;
+    attachInterrupt(RPM_InterruptPort, RPM_CountInterrupt, FALLING);
+  }
+/*
+  if (!cruiseON)
+  {
+    sensorValue = analogRead(sensorPin);
+    sensorValue = analogRead(sensorPin);
+  }*/
 
 
-  myGLCD.printNumI(currentSpeed, 351, 72, 4, '0');
-  myGLCD.printNumI((int)currentSpeed * speedConversionValue, 327, 144, 4, '0');
-  myGLCD.printNumI(sensorValue, 351, 220, 4, '0');
+  myGLCD.printNumI(currentSpeed, 351, 72, 2, '0');
+  myGLCD.printNumF(currentSpeed*speedConversionValue,1, 327, 144);
 #if TOUCH
   if (myTouch.dataAvailable() == true)
   {
@@ -176,19 +174,23 @@ void loop()
       if (selected != choice1)
         ptr = label;
       myButtons.relabelButton(selected, ptr, true);
-      myGLCD.print(ptr, 334, 290);
+      myGLCD.print(ptr, 334, 216);
     }
   }
 #endif
   if (getTemperaturesBtn)
   {
     getTemperatures();
-    for (int i = 0; i < 5; i++)
+    for(int i = 0; i < 5; i++)
     {
-      myGLCD.setFont(BigFont);
-      myGLCD.printNumF(temperatures[i], 1, (i * 120), 335);
-      myGLCD.setFont(Ubuntu);
+      Serial.print(temperatures[i],1);
+      Serial.print('\t');
     }
+    Serial.print(currentSpeed,1);
+    Serial.print('\t');
+    Serial.print(full_revolutions);
+    Serial.print('\t');
+    Serial.println(currentSpeed*speedConversionValue);
   }
 
 }
@@ -199,11 +201,12 @@ void getTemperatures()
   for (int var = 0; var < 5; var++) {
     temperaturesInt[var] = 0;
   }
-  myGLCD.print("                         ", 0, 335);
+  myGLCD.print("                         ",0, 335);
   for (int j = 0; j < 5; j++)
   {
     Serial3.write(48 + j);
     temperatures[j] = Serial3.parseFloat();
+    myGLCD.printNumF(temperatures[j], 1, (j * 120), 335);
   }
   getTemperaturesBtn = 0;
 }
@@ -223,11 +226,5 @@ void TC4_Handler()
 //revolutions counter
 void RPM_CountInterrupt()
 {
-  static int currentMillis = 0;
   full_revolutions++;
-  currentMillis = millis();
-  delta = currentMillis - timeold;
-  currentSpeed = (60 * 1000 / (currentMillis - timeold));// * full_revolutions); //*speedConversionValue;
-  timeold = currentMillis;
-
 }
