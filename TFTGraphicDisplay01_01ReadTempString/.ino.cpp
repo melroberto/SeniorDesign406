57b63e641543956bf4993d2a6e8fb6bf5b0251b5
Roberto Melendez <roberto.melendez@gmail.com>
2016-12-02 23:51:34 -0500

    .ino.cpp file updated

--- TFTGraphicDisplay01_01ReadTempString/.ino.cpp
+++ TFTGraphicDisplay01_01ReadTempString/.ino.cpp
@@ -2,7 +2,7 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2016-11-30 01:20:48
+//This file has been generated on 2016-12-02 23:50:57
 
 #include "Arduino.h"
 #include <SD.h>
@@ -10,7 +10,6 @@
 #include <UTFT.h>
 #include <URTouch.h>
 #include <UTFT_Buttons.h>
-int PIDUpdate(int sensorValue, float currentRPM, int sampleMillis) ;
 void startTimer(Tc *tc, uint32_t channel, IRQn_Type irq, uint32_t frequency) ;
 void setup() ;
 void loop() ;
